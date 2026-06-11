a8610793c861e0dfce7a2e5f43cc21657c503c11
Gabriely <gabno971@gmail.com>
2026-06-10 22:05:22 -0300

    deploy 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,5 +17,4 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->preventRequestForgery();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
     })->create();
